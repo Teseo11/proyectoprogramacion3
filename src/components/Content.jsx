449280baf65e15fec938199de0ce05bf1883e5f3
Teseo <danicabral15@outlook.com>
2023-07-02 18:01:03 -0300

    q

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,9 @@
-import ComponentsRoutes from './ComponentsRoutes'
+import ComponentsRoutes from "./ComponentsRoutes";
 
 function Content() {
-<<<<<<< HEAD
   const { rol, isAuthenticated } = useAuth();
 
-=======
->>>>>>> 4b0e4ba22299bcd87aed40be1e3016d892120f6f
-  return (
-     <ComponentsRoutes />
-  )
+  return <ComponentsRoutes />;
 }
 
-export default Content
+export default Content;
