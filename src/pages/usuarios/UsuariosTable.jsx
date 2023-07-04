a77583a467d9074744fc5e2acadfcdd0ccb5f36c
maximoaparicio <maxcodingg@gmail.com>
2023-07-03 21:10:45 -0300

    styles changes

--- src/pages/usuarios/UsuariosTable.jsx
+++ src/pages/usuarios/UsuariosTable.jsx
@@ -1,91 +1,81 @@
-import { useState, useEffect } from "react";
-import { Table, Button, Space } from "antd";
-import { Link } from "react-router-dom";
-import { useEntities } from "../../context/EntitiesContext";
-import { faTrash, faPencil, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useEffect } from 'react'
+import { Table, Button, Space } from 'antd'
+import { Link, NavLink } from 'react-router-dom'
+import { useEntities } from '../../context/EntitiesContext'
+import { faTrash, faPencil, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { HiOutlineUserAdd, HiOutlinePencilAlt, HiOutlineTrash } from 'react-icons/hi'
+import '../Table.css'
 
 const UsuariosTable = () => {
-  const [data, setData] = useState([]);
-  const { getElement, deleteElement } = useEntities();
+  const [data, setData] = useState([])
+  const { getElement, deleteElement } = useEntities()
 
   useEffect(() => {
     const getClientes = async () => {
-      const res = await getElement("/usuarios");
+      const res = await getElement('/usuarios')
       setData(
-        res.map((row) => ({
+        res.map(row => ({
           id: row.id,
           Username: row.username,
           Rol: row.rol,
         }))
-      );
-    };
+      )
+    }
 
-    getClientes();
-  }, [data]);
+    getClientes()
+  }, [data])
 
   const generateColumns = () => {
     if (data.length === 0) {
-      return []; // No hay datos disponibles, no se generan columnas
+      return [] // No hay datos disponibles, no se generan columnas
     }
 
     // Obtener las claves (nombres de los campos) del primer objeto en los datos
-    const keys = Object.keys(data[0]);
+    const keys = Object.keys(data[0])
 
     // Generar las columnas dinámicamente utilizando map
-    const columns = keys.map((key) => ({
+    const columns = keys.map(key => ({
       title: key, // Utilizar el nombre del campo como título de la columna
       dataIndex: key, // Utilizar el nombre del campo como dataIndex
       key, // Utilizar el nombre del campo como clave
-    }));
+    }))
 
     // Agregar las columnas de los botones de eliminar y editar
     columns.push({
-      title: "Acciones",
-      key: "actions",
-      fixed: "right",
+      title: 'Acciones',
+      key: 'actions',
+      fixed: 'right',
       render: (_, record) => (
         <Space>
           <Link to={`./edit/${record.id}`}>
-            <Button>
-              <FontAwesomeIcon
-                icon={faPencil}
-                size="xl"
-                style={{ color: "#65c374" }}
-              />
-            </Button>
+              <HiOutlinePencilAlt size={30} color='#FCA311'/>
           </Link>
-          <Button
+          <Link
             type="primary"
             danger
-            onClick={() => deleteElement("/usuarios/", record.id)}
+            onClick={() => deleteElement('/usuarios/', record.id)}
           >
-            <FontAwesomeIcon icon={faTrash} size="xl" />
-          </Button>
+            <HiOutlineTrash size={30} color='#FCA311'/>
+          </Link>
         </Space>
       ),
-    });
+    })
 
-    return columns;
-  };
+    return columns
+  }
 
   return (
-    <>
-      <Link to="./new">
-        <Button
-          type="primary"
-          style={{ margin: "10px 10px 0px 10px", height: "50px" }}
-        >
-          <FontAwesomeIcon icon={faPlus} beat size="2xl" />
-        </Button>
-      </Link>
-      <Table
-        dataSource={data}
-        columns={generateColumns()}
-        pagination={{ pageSize: 5, total: 10, showSizeChanger: true }}
-      />
-    </>
-  );
-};
+    <div className="container w-75 mt-5">
+      <div className="add-section">
+        <NavLink to="./new">
+          <HiOutlineUserAdd size={30} color='#FCA311'/>
+          Añadir nuevo usuario
+        </NavLink>
+      </div>
+      <Table dataSource={data} columns={generateColumns()} />
+    </div>
+  )
+}
 
-export default UsuariosTable;
+export default UsuariosTable
